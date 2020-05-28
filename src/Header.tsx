05b70ab283f246ea43c5b698a884d24f57f8edff
Kyome22 <kyomesuke@gmail.com>
2020-05-28 14:28:41 +0900

    fix eslint

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 type Props = {
-    language: string;
-    profile: string;
-    works: string;
-    articles: string;
-    contact: string;
+  language: string;
+  profile: string;
+  works: string;
+  articles: string;
+  contact: string;
 };
 
+const Test = () => (
+  <div>
+    <p>Test</p>
+  </div>
+);
+
 export function Header(props: Props) {
-    const {
-        language,
-        profile,
-        works,
-        articles,
-        contact
-    } = props;
+  const { language, profile, works, articles, contact } = props;
 
-    // handleLanguage = () => {
-    //     this.setState({
-    //         localize
-    //     })
-    // }
+  // handleLanguage = () => {
+  //     this.setState({
+  //         localize
+  //     })
+  // }
 
-    return (<div className="header">
-    <div className="left-elements">
-    <h2 className="title">Takuto Nakamura's Portfolio</h2>
+  return (
+    <div className="header">
+      <div className="left-elements">
+        <h2 className="title">Takuto Nakamura's Portfolio</h2>
+      </div>
+
+      <div className="right-elements">
+        <h2>{language}</h2>
+        <h2>{profile}</h2>
+        <h2>{works}</h2>
+        <h2>{articles}</h2>
+        <h2>{contact}</h2>
+      </div>
     </div>
-    <div className="right-elements">
-    <h2>{language}</h2>
-    <h2>{profile}</h2>
-    <h2>{works}</h2>
-    <h2>{articles}</h2>
-    <h2>{contact}</h2>
-    </div>
-    </div>
-    );
+  );
 }
